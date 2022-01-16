4b85843cac82b89d75a2fb9a9e42144fce4f8824
Robert <robertu.dev@gmail.com>
2022-01-15 23:55:08 -0600

    created homepage & responsive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 
 //Top of Page Button (index.html)
 
-var ua = navigator.userAgent.toLowerCase(); 
-if (ua.indexOf('safari') != -1) { 
+var ua = navigator.userAgent.toLowerCase();
+if (ua.indexOf('safari') != -1) {
   if (ua.indexOf('chrome') > -1) {
   } else {
-document.getElementById("btn1").style.marginTop = "-9%";  }
-}  
+    document.getElementById("btn1").style.marginTop = "-9%";
+  }
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
 
 
