1cb9627f8947e2cebb9628b603a0474cf8dff9b3
Robert <robertu.dev@gmail.com>
2022-01-19 01:07:58 -0600

    created about me page

--- js/main.js
+++ js/main.js
@@ -15,11 +15,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.parallax');
   var instances = M.Parallax.init(elems);
 });
